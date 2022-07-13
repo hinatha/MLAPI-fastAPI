1f8df8f785d06b6bb49445fa616f1135235d270a
hinatha <hinatha0110@gmail.com>
2022-07-13 13:02:30 +0900

    Finished read image info

--- api/preparations/image.py
+++ api/preparations/image.py
@@ -1,24 +1,41 @@
 import uuid
 import boto3
 from fastapi import UploadFile, Depends
+from typing import List
 
 from functools import lru_cache
 from werkzeug.utils import secure_filename
 
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from sqlalchemy.engine import Result
 
 from api import config
-from api.models.image import ImageInfo
-from api.schemas.image import FileIdModel
+import api.models.image as image_model
+import api.schemas.image as image_schema
 
 
+# Return all image info
+async def get_images(db: AsyncSession) -> List[image_schema.ImageList]:
+    result: Result = await (
+        db.execute(
+            select(
+                image_model.ImageInfo.file_id,
+                image_model.ImageInfo.filename,
+            )
+        )
+    )
+    return result.all()
+
+
+# Setting S3 info
 @lru_cache()
 def get_settings():
     return config.Settings()
 
-async def insert_filedata(
-    files: list[UploadFile], db: AsyncSession
-) -> FileIdModel:
+
+# Post image file, file name as metadata and return file_id
+async def insert_filedata(files: list[UploadFile], db: AsyncSession) -> image_schema.FileIdModel:
     """手書き文字画像のファイル名をデータベースに保存し、ファイルをS3のフォルダにアップロードする"""
     file_id = str(uuid.uuid4())
 
@@ -43,11 +60,12 @@ async def insert_filedata(
     # Execute files upload to S3
     for file in files:
         filename = secure_filename(file.filename)
-        image_info = ImageInfo(file_id=file_id, filename=filename)
+        image_info = image_model.ImageInfo(file_id=file_id, filename=filename)
         db.add(image_info)
         key = dir_name + filename
         s3_client.put_object(Body=file.file, Bucket=bucket_name, Key=key)
 
     await db.commit()
+
     file_id_json = {"file_id": file_id}
     return file_id_json
--- api/routers/image.py
+++ api/routers/image.py
@@ -4,10 +4,9 @@ from fastapi import APIRouter, UploadFile, File, Depends
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api.files import preparation
 
 from api.db import get_db
-
+import api.preparations.image as image_preparation
 import api.schemas.image as image_schema
 
 
@@ -15,12 +14,12 @@ router = APIRouter()
 
 # Show list of image id and images names
 @router.get("/images", response_model=List[image_schema.ImageList])
-async def list_images():
-    return [image_schema.ImageList(file_id=1, filename="0.jpg")]
+async def list_images(db: AsyncSession = Depends(get_db)):
+    return await image_preparation.get_images(db)
 
 # Save picture file to S3 and filename to DB and return file_id.
 @router.post("/images", response_model=image_schema.FileIdModel)
 async def upload_images(
     files: list[UploadFile] = File(description="Multiple files as UploadFile"), db: AsyncSession = Depends(get_db)
 ):
-    return await preparation.insert_filedata(files, db)
+    return await image_preparation.insert_filedata(files, db)
